517cf9d847b2c4469a74cfb372ba478512237dbd
lokhunliao <35357272+lokhunliao@users.noreply.github.com>
2021-02-21 16:41:16 -0800

    BACKEND

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://localhost:27017/campsite',
+    'facebook' : {
+        clientId: '240472240959692',
+        clientSecret: '5cce8f3497194f8b4127a567064b160f'
+    }
+}
